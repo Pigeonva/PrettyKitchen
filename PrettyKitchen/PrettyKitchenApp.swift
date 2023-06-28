6db9768603a395d6a34cf29b0763ecbae062b284
PigeonVA <i-realman@mail.ru>
2023-06-28 11:30:34 +0300

    Initial Commit

---
+++ PrettyKitchen/PrettyKitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrettyKitchenApp.swift
+//  PrettyKitchen
+//
+//  Created by Артур Фомин on 28.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PrettyKitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
